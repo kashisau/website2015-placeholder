a70068df403943e1aa52bfe0b14dcb797fc2c420
Kashi Samaraweera <kashi.samaraweera@ward6.com.au>
2015-07-13 17:46:17 +1000

    Working on transforming contact form.
    Former-commit-id: 3e7631431f3fead5e574cead99487cac7b292a7d

--- source/scripts/placeholder.js
+++ source/scripts/placeholder.js
@@ -6,8 +6,20 @@
  *     https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
  **/
 
-if (! (document.cookie.replace(/(?:(?:^|.*;\s*)hasVisitedBefore\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true")) {
-    var placeholder = document.querySelector('.Placeholder');
-    placeholder.className = placeholder.className + ' Placeholder--dontAnimate';
-}
-document.cookie = "hasVisitedBefore=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+var cookie = function() {
+    if (!(document.cookie.replace(/(?:(?:^|.*;\s*)hasVisitedBefore\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true")) {
+        var placeholder = document.querySelector('.Placeholder');
+        placeholder.className = placeholder.className + ' Placeholder--dontAnimate';
+    }
+    document.cookie = "hasVisitedBefore=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+}();
+
+var contact = function() {
+    var contactForm = document.querySelector('.ContactForm');
+    
+    contactForm.addEventListener('click', function(toggleFormEvent) {
+        var contactForm = toggleFormEvent.currentTarget;
+        contactForm.classList.toggle('ContactForm--expanded');
+    });
+    
+}();
