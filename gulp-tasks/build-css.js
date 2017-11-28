6d789597a2b99741dd23b8bc1ff23cb76ad24d03
Kashi Samaraweera <kashi@kashis.com.au>
2017-11-28 07:24:30 +0100

    Updated copy and sourcemap URI.

--- gulp-tasks/build-css.js
+++ gulp-tasks/build-css.js
@@ -25,7 +25,7 @@ module.exports = function(gulp, plugins, production) {
             reworkCustomMedia = require('rework-custom-media');
 
         var development = !production;
-        var REPO_SRC_URL = 'https://bitbucket.org/KashiS/website2015-placeholder/raw/master/source/styles/';
+        var REPO_SRC_URL = 'https://raw.githubusercontent.com/kashisau/website2015-placeholder/master/source/styles/';
 
         /* Lazy pipes for CSS */
         /**
